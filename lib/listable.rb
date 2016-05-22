c188652097c890d5b2fe1520a1818ea21ab40c60
Paul Haddad <paulh16@gmail.com>
2016-05-22 09:05:15 -0500

    Extract #format_date and #format_priority methods

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,4 +2,22 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_date(start_date, end_date = nil)
+    if start_date && end_date
+      "#{start_date.strftime("%D")} -- #{end_date.strftime("%D")}"
+    elsif start_date
+      "#{start_date.strftime("%D")}"
+    else
+      "N/A"
+    end
+  end
+
+  def format_priority(priority)
+    value = " ⇧" if priority == "high"
+    value = " ⇨" if priority == "medium"
+    value = " ⇩" if priority == "low"
+    value = "" if !priority
+    return value
+  end
 end
